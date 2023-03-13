5e9702ceab5576b042a6c48723dceaca531c327e
Mathieu Moulin <mathieu@iprospective.fr>
2023-03-13 20:29:07 +0100

    Initialisation

---
+++ class/actions_mmifournisseurprice.class.php
@@ -0,0 +1,11 @@
+<?php
+
+dol_include_once('custom/mmicommon/class/mmi_actions.class.php');
+
+class ActionsMMIFournisseurPrice extends MMI_Actions_1_0
+{
+	const MOD_NAME = 'mmifournisseurprice';
+	
+}
+
+ActionsMMIFournisseurPrice::__init();
